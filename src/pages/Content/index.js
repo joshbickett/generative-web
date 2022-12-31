2dc05428d3a4be7211236202aa01d6f9be04776e
Josh Bickett <josh@bickett.net>
2022-12-31 05:56:16 -0800

    Remove new tab functionality for now

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,4 @@
 import { printLine } from './modules/print';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+console.log('Content script works! It should only load on Google.');
 
-printLine("Using the 'printLine' function from the Print Module");
