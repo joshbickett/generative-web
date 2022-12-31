6addc29fa3ce987f1fc0f71d675f7a7edf7c321b
Josh Bickett <josh@bickett.net>
2022-12-31 06:07:28 -0800

    Add an app component

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,4 +1,16 @@
 import { printLine } from './modules/print';
+import ReactDOM from 'react-dom';
 
 console.log('Content script works! It should only load on Google.');
 
+const App = () => {
+  return <div id="extension-ui"></div>;
+};
+
+setTimeout(() => {
+  const root = document.createElement('div');
+  root.id = 'generative-web';
+
+  document.body.appendChild(root);
+  ReactDOM.render(<App />, root);
+}, 500);
